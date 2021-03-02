2a348875085dc59a0a95d875cede64e1e5712cae
Mathias Halén <matte_halen@hotmail.com>
2021-03-02 16:08:04 +0100

    Initial Commit

---
+++ Scheduled-countdown WatchKit Extension/NotificationController.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationController.swift
+//  Scheduled-countdown WatchKit Extension
+//
+//  Created by Administrator on 2021-03-02.
+//  Copyright © 2021 Mathias Halén. All rights reserved.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+
+    override var body: NotificationView {
+        return NotificationView()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
---
+++ Scheduled-countdown WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Scheduled-countdown WatchKit Extension
+//
+//  Created by Administrator on 2021-03-02.
+//  Copyright © 2021 Mathias Halén. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
